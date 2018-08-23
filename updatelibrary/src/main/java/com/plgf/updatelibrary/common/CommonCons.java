f98af2dcee69d69b31d4cd090ac61d5e13911587
PilotDeveloperAccount <1038883524@qq.com>
2018-08-23 15:30:14 +0800

    Initial commit

---
+++ updatelibrary/src/main/java/com/plgf/updatelibrary/common/CommonCons.java
@@ -0,0 +1,21 @@
+package com.plgf.updatelibrary.common;
+
+import android.graphics.Bitmap;
+
+import java.io.File;
+
+/**
+ * Created by maimingliang on 2016/10/8.
+ */
+
+public class CommonCons {
+
+    public static final double version = 1.3;
+    public final static String SAVE_APP_NAME = "download"+version+".apk";
+
+    public final static String SAVE_APP_LOCATION = "/download";
+
+
+    public final static String APP_FILE_NAME = "/sdcard"+SAVE_APP_LOCATION+ File.separator + SAVE_APP_NAME;
+    public static final String DOWNLOAD_APK_ID_PREFS = "download_apk_id_prefs";
+}
