263f84c4dd93248498d8e14d04f190ba960fa96b
PilotDeveloperAccount <1038883524@qq.com>
2018-08-23 15:31:57 +0800

    测试

--- updatelibrary/src/main/java/com/plgf/updatelibrary/common/CommonCons.java
+++ updatelibrary/src/main/java/com/plgf/updatelibrary/common/CommonCons.java
@@ -16,6 +16,7 @@ public class CommonCons {
     public final static String SAVE_APP_LOCATION = "/download";
 
 
+
     public final static String APP_FILE_NAME = "/sdcard"+SAVE_APP_LOCATION+ File.separator + SAVE_APP_NAME;
     public static final String DOWNLOAD_APK_ID_PREFS = "download_apk_id_prefs";
 }
